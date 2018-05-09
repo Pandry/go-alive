e61addade2e0cbb567fed3338e99e5accf4f43c7
Pandry <andry25.9.99@gmail.com>
2018-05-09 22:53:31 +0200

    Good working prototype

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/sparrc/go-ping"
 )
 
@@ -17,16 +22,44 @@ type pingReply struct {
 }
 
 func main() {
-	IPs := [...]string{"8.8.8.8", "8.8.4.4", "8.4.8.4", "192.168.1.100", "10.0.0.1", "10.0.1.1", "10.1.1.1"}
-	var pingRes pingReply
-	for _, ip := range IPs {
-		pingRes = pingIP(ip)
 
-		av := "TANGO DOWN!"
-		if pingRes.Reachable {
-			av = "IT'S ALIVE!"
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		for sig := range c {
+			log.Printf("captured %v, stopping profiler and exiting..", sig)
+			os.Exit(1)
 		}
-		fmt.Println("Ping result from " + pingRes.Source + ": " + av)
+	}()
+
+	//Init bot
+	bot, err := tgbotapi.NewBotAPI("144847736:AAGWBPiFCaejWlFCIW7bnsaNfB3_NUlLqV0")
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = false
+
+	IPs := [...]string{"8.8.8.8", "8.8.4.4", "8.4.8.4", "192.168.1.100", "10.0.0.1", "10.0.1.1", "10.1.1.1"}
+	tgAdmins := [...]int64{14092073}
+
+	var pingRes pingReply
+
+	for true {
+		for _, ip := range IPs {
+
+			pingRes = pingIP(ip)
+
+			av := "IT'S ALIVE!"
+			if !pingRes.Reachable {
+				av = "TANGO DOWN!"
+				for _, admin := range tgAdmins {
+					bot.Send(tgbotapi.NewMessage(admin, ip+" - TANGO DOWN!"))
+				}
+			}
+			fmt.Println("Ping result from " + pingRes.Source + ": " + av)
+		}
+		fmt.Println("IPs finished, rechecking in 20 seconds")
+		time.Sleep(time.Second * 20)
 	}
 
 }
@@ -50,7 +83,5 @@ func pingIP(ip string) pingReply {
 		reply.Reachable = true
 	}
 	reply.Latency = stats.AvgRtt
-	//fmt.Println("Result from IP " + ip + ":")
-	//fmt.Println(reply)
 	return reply
 }
