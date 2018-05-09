ef5cf67f7ea4aac487492a78096c19bf6258368b
Pandry <andry25.9.99@gmail.com>
2018-05-09 22:24:02 +0200

    First good example

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/sparrc/go-ping"
@@ -18,52 +17,21 @@ type pingReply struct {
 }
 
 func main() {
+	IPs := [...]string{"8.8.8.8", "8.8.4.4", "8.4.8.4", "192.168.1.100", "10.0.0.1", "10.0.1.1", "10.1.1.1"}
+	var pingRes pingReply
+	for _, ip := range IPs {
+		pingRes = pingIP(ip)
 
-	/*
-		pinger, err := ping.NewPinger("www.google.com")
-		if err != nil {
-			panic(err)
-		}
-		pinger.SetPrivileged(true)
-		pinger.Timeout = time.Second * 30
-
-		pinger.OnRecv = func(pkt *ping.Packet) {
-			fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v\n",
-				pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt)
-		}
-		pinger.OnFinish = func(stats *ping.Statistics) {
-			fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
-			fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
-				stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
-			fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
-				stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
-		}
-
-		fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
-		pinger.Run()
-	*/
-
-	messages := make(chan pingReply)
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go pingIP("8.8.8.0", &wg, messages)
-	wg.Add(1)
-	go pingIP("8.8.8.9", &wg, messages)
-	wg.Add(1)
-	go pingIP("8.8.8.8", &wg, messages)
-	wg.Wait()
-	for i := range messages {
 		av := "TANGO DOWN!"
-		if i.Reachable {
+		if pingRes.Reachable {
 			av = "IT'S ALIVE!"
 		}
-		fmt.Println("Ping result from " + i.Source + ": " + av)
+		fmt.Println("Ping result from " + pingRes.Source + ": " + av)
 	}
 
 }
 
-func pingIP(ip string, wg *sync.WaitGroup, ch chan pingReply) pingReply {
-	defer wg.Done()
+func pingIP(ip string) pingReply {
 	reply := pingReply{Reachable: false, Source: ip}
 	iping, err := ping.NewPinger(ip)
 	if err != nil {
@@ -71,7 +39,6 @@ func pingIP(ip string, wg *sync.WaitGroup, ch chan pingReply) pingReply {
 		fmt.Println("Error!")
 		fmt.Println(err)
 		reply.Error = err
-		wg.Done()
 		return reply
 	}
 	iping.SetPrivileged(true)
@@ -85,6 +52,5 @@ func pingIP(ip string, wg *sync.WaitGroup, ch chan pingReply) pingReply {
 	reply.Latency = stats.AvgRtt
 	//fmt.Println("Result from IP " + ip + ":")
 	//fmt.Println(reply)
-	ch <- reply
 	return reply
 }
